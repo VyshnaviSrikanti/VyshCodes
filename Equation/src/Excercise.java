66d56b44d892c2a78687625487f5ffc08c26e87f
harsh <harsh@192.168.0.149>
2021-04-07 10:19:10 +0200

    Git Practice

---
+++ Equation/src/Excercise.java
@@ -0,0 +1,13 @@
+
+public class Excercise {
+	public static void main(String args[]){
+		
+		//(a+b)square= (a*a) + (b*b) +(2*a*b)
+		int a=25;
+		float b=42.125f;
+		int   value= (int)( (a*a) + (b*b) +(2*a*b));
+		System.out.println(value);
+	
+	}
+
+}
